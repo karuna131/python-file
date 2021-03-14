aa9c723b115eaf2ba402031e97983b99886c8984
karuna131 <78745446+karuna131@users.noreply.github.com>
2021-03-14 12:36:08 +0530

    Add files via upload

---
+++ people1.py
@@ -0,0 +1,43 @@
+# simple 
+
+# my_file = open("people1.txt")
+# file = my_file.read()
+# print(file)
+# my_file.close()
+
+
+
+#bina mode (mean w) ye write nhi krega 
+
+# a=open("people1.txt","w")
+# a.write("I am Karuna")
+# a.close()
+
+
+
+#new file create karega
+
+# a = open("people1.txt","w")
+# a.write("I am going to home")
+# a.close()
+
+
+
+
+#string count karega
+# my_file = open("people1.txt", "w")
+# file=my_file.write("I am karuna\n")
+# m=my_file.write("I am saloni")
+# print(file,m)
+# my_file.close()
+
+
+
+
+
+# add krne ke lie
+
+# a=open("people1.txt", "a")
+# a.write("\nI am karuna\n")
+# a.write("I am from MP")
+# a.close()
